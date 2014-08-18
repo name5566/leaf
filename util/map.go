9d69229dc9a7ea268a9a6733f432fd728583b95d
name5566 <name5566@gmail.com>
2014-08-18 10:16:42 +0800

    fix map init error

--- util/map.go
+++ util/map.go
@@ -9,20 +9,35 @@ type Map struct {
 	m map[interface{}]interface{}
 }
 
+func (m *Map) init() {
+	if m.m == nil {
+		m.m = make(map[interface{}]interface{})
+	}
+}
+
 func (m *Map) Get(key interface{}) interface{} {
 	m.RLock()
 	defer m.RUnlock()
-	return m.m[key]
+
+	if m.m == nil {
+		return nil
+	} else {
+		return m.m[key]
+	}
 }
 
 func (m *Map) Set(key interface{}, value interface{}) {
 	m.Lock()
 	defer m.Unlock()
+
+	m.init()
 	m.m[key] = value
 }
 
 func (m *Map) Del(key interface{}) {
 	m.Lock()
 	defer m.Unlock()
+
+	m.init()
 	delete(m.m, key)
 }
