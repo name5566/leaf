11c2a46be18fa5ccb81edae9d6391495b8bff156
name5566 <name5566@gmail.com>
2014-12-11 16:19:05 +0800

    add call router example

--- util/example_test.go
+++ util/example_test.go
@@ -3,6 +3,7 @@ package util_test
 import (
 	"fmt"
 	"github.com/name5566/leaf/util"
+	"sync"
 )
 
 func ExampleMap() {
@@ -24,5 +25,42 @@ func ExampleMap() {
 	// <nil>
 	// value
 	// <nil>
+	// 3
+}
+
+func ExampleCallRouter() {
+	r := util.NewCallRouter(10)
+	var wg sync.WaitGroup
+
+	// goroutine 1
+	wg.Add(1)
+	go func() {
+		// def
+		r.Def("add", func(args []interface{}) interface{} {
+			num1 := args[0].(int)
+			num2 := args[1].(int)
+			return num1 + num2
+		})
+
+		// route
+		ci := <-r.Chan()
+		r.Route(ci)
+
+		wg.Done()
+	}()
+
+	// goroutine 2
+	wg.Add(1)
+	go func() {
+		// call
+		c := r.Call1("add", 1, 2)
+		fmt.Println(<-c)
+
+		wg.Done()
+	}()
+
+	wg.Wait()
+
+	// Output:
 	// 3
 }
