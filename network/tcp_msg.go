d3877f21029a2cb52da9fc76cb8e223eb047af0d
name5566 <name5566@gmail.com>
2014-12-16 15:00:22 +0800

    add msg parser.

---
+++ network/tcp_msg.go
@@ -0,0 +1,142 @@
+package network
+
+import (
+	"encoding/binary"
+	"errors"
+	"io"
+	"math"
+)
+
+// --------------
+// | len | data |
+// --------------
+type MsgParser struct {
+	lenMsgLen    int
+	minMsgLen    uint32
+	maxMsgLen    uint32
+	littleEndian bool
+}
+
+func NewMsgParser(lenMsgLen int, minMsgLen uint32, maxMsgLen uint32, littleEndian bool) *MsgParser {
+	if lenMsgLen != 1 || lenMsgLen != 2 || lenMsgLen != 4 {
+		lenMsgLen = 2
+	}
+	if minMsgLen == 0 {
+		minMsgLen = 1
+	}
+	if maxMsgLen == 0 {
+		maxMsgLen = 4096
+	}
+
+	var max uint32
+	switch lenMsgLen {
+	case 1:
+		max = math.MaxUint8
+	case 2:
+		max = math.MaxUint16
+	}
+	if minMsgLen > max {
+		minMsgLen = max
+	}
+	if maxMsgLen > max {
+		maxMsgLen = max
+	}
+
+	p := new(MsgParser)
+	p.lenMsgLen = lenMsgLen
+	p.minMsgLen = minMsgLen
+	p.maxMsgLen = maxMsgLen
+	p.littleEndian = littleEndian
+
+	return p
+}
+
+func (p *MsgParser) Read(conn *TCPConn) ([]byte, error) {
+	var b [4]byte
+	bufMsgLen := b[:p.lenMsgLen]
+
+	// read len
+	if _, err := io.ReadFull(conn, bufMsgLen); err != nil {
+		return nil, err
+	}
+
+	// parse len
+	var msgLen uint32
+	switch p.lenMsgLen {
+	case 1:
+		msgLen = uint32(bufMsgLen[0])
+	case 2:
+		if p.littleEndian {
+			msgLen = uint32(binary.LittleEndian.Uint16(bufMsgLen))
+		} else {
+			msgLen = uint32(binary.BigEndian.Uint16(bufMsgLen))
+		}
+	case 4:
+		if p.littleEndian {
+			msgLen = binary.LittleEndian.Uint32(bufMsgLen)
+		} else {
+			msgLen = binary.BigEndian.Uint32(bufMsgLen)
+		}
+	}
+
+	// check len
+	if msgLen > p.maxMsgLen {
+		return nil, errors.New("message too long")
+	} else if msgLen < p.minMsgLen {
+		return nil, errors.New("message too short")
+	}
+
+	// data
+	msgData := make([]byte, msgLen)
+	if _, err := io.ReadFull(conn, msgData); err != nil {
+		return nil, err
+	}
+
+	return msgData, nil
+}
+
+func (p *MsgParser) Write(conn *TCPConn, args ...[]byte) error {
+	// get len
+	var msgLen uint32
+	for i := 0; i < len(args); i++ {
+		msgLen += uint32(len(args[i]))
+	}
+
+	// check len
+	if msgLen > p.maxMsgLen {
+		return errors.New("message too long")
+	} else if msgLen < p.minMsgLen {
+		return errors.New("message too short")
+	}
+
+	msg := make([]byte, uint32(p.lenMsgLen)+msgLen)
+
+	// write len
+	switch p.lenMsgLen {
+	case 1:
+		msg[0] = byte(msgLen)
+	case 2:
+		if p.littleEndian {
+			binary.LittleEndian.PutUint16(msg, uint16(msgLen))
+		} else {
+			binary.BigEndian.PutUint16(msg, uint16(msgLen))
+		}
+	case 4:
+		if p.littleEndian {
+			binary.LittleEndian.PutUint32(msg, msgLen)
+		} else {
+			binary.BigEndian.PutUint32(msg, msgLen)
+		}
+	}
+
+	// write data
+	l := p.lenMsgLen
+	for i := 0; i < len(args); i++ {
+		copy(msg[l:], args[i])
+		l += len(args[i])
+	}
+
+	conn.Write(msg)
+
+	return nil
+}
