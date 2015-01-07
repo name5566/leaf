d271a82dc6e9ac32585c32df2a35f2bc07caa7e8
name5566 <name5566@gmail.com>
2015-01-07 13:32:00 +0800

    impl semaphore

---
+++ util/semaphore.go
@@ -0,0 +1,15 @@
+package util
+
+type Semaphore chan struct{}
+
+func NewSemaphore(n int) Semaphore {
+	return make(Semaphore, n)
+}
+
+func (s Semaphore) Acquire() {
+	s <- struct{}{}
+}
+
+func (s Semaphore) Release() {
+	<-s
+}
