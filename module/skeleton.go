fd376d7ebe60f790652c32d05472bd523294d09a
name5566 <name5566@gmail.com>
2015-02-02 23:25:04 +0800

    redesign skeleton

--- module/skeleton.go
+++ module/skeleton.go
@@ -9,67 +9,71 @@ import (
 )
 
 type Skeleton struct {
-	Name               string
-	ChanRPCServerLen   int
 	GoLen              int
 	TimerDispatcherLen int
 	ChanRPCServer      *chanrpc.Server
 	g                  *g.Go
-	timerDispatcher    *timer.Dispatcher
+	dispatcher         *timer.Dispatcher
+	server             *chanrpc.Server
 }
 
-func (s *Skeleton) InitSkeleton() {
-	if s.Name == "" {
-		s.Name = "Unnamed"
-	}
-
-	if s.ChanRPCServerLen <= 0 {
-		s.ChanRPCServerLen = 10000
-		log.Release("invalid ChanRPCServerLen, reset to %v", s.ChanRPCServerLen)
-	}
-	s.ChanRPCServer = chanrpc.NewServer(s.ChanRPCServerLen)
-
+func (s *Skeleton) Init() {
 	if s.GoLen <= 0 {
-		s.GoLen = 10000
-		log.Release("invalid GoLen, reset to %v", s.GoLen)
+		s.GoLen = 0
 	}
-	s.g = g.New(s.GoLen)
-
 	if s.TimerDispatcherLen <= 0 {
-		s.TimerDispatcherLen = 10000
-		log.Release("invalid TimerDispatcherLen, reset to %v", s.TimerDispatcherLen)
+		s.TimerDispatcherLen = 0
 	}
-	s.timerDispatcher = timer.NewDispatcher(s.TimerDispatcherLen)
+
+	s.g = g.New(s.GoLen)
+	s.dispatcher = timer.NewDispatcher(s.TimerDispatcherLen)
+	s.server = s.ChanRPCServer
+
+	if s.server == nil {
+		s.server = chanrpc.NewServer(0)
+	}
 }
 
-func (s *Skeleton) RunSkeleton(closeSig chan bool) {
+func (s *Skeleton) Run(closeSig chan bool) {
 	for {
 		select {
 		case <-closeSig:
-			s.ChanRPCServer.Close()
+			s.server.Close()
 			s.g.Close()
 			return
-		case ci := <-s.ChanRPCServer.ChanCall:
-			err := s.ChanRPCServer.Exec(ci)
+		case ci := <-s.server.ChanCall:
+			err := s.server.Exec(ci)
 			if err != nil {
-				log.Error("%v module chanrpc error: %v", s.Name, err)
+				log.Error("chanrpc error: %v", err)
 			}
 		case cb := <-s.g.ChanCb:
 			s.g.Cb(cb)
-		case t := <-s.timerDispatcher.ChanTimer:
+		case t := <-s.dispatcher.ChanTimer:
 			t.Cb()
 		}
 	}
 }
 
 func (s *Skeleton) AfterFunc(d time.Duration, cb func()) *timer.Timer {
-	return s.timerDispatcher.AfterFunc(d, cb)
+	if s.TimerDispatcherLen == 0 {
+		panic("invalid TimerDispatcherLen")
+	}
+
+	return s.dispatcher.AfterFunc(d, cb)
 }
 
 func (s *Skeleton) Go(f func(), cb func()) {
+	if s.GoLen == 0 {
+		panic("invalid GoLen")
+	}
+
 	s.g.Go(f, cb)
 }
 
 func (s *Skeleton) RegisterChanRPC(id interface{}, f interface{}) {
-	s.ChanRPCServer.Register(id, f)
+	if s.ChanRPCServer == nil {
+		panic("invalid ChanRPCServer")
+	}
+
+	s.server.Register(id, f)
 }
