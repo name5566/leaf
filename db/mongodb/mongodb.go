473132bf696b138b7f8add73e016d13d36054dd4
name5566 <name5566@gmail.com>
2015-02-13 10:55:45 +0800

    auto increment and index

--- db/mongodb/mongodb.go
+++ db/mongodb/mongodb.go
@@ -4,6 +4,7 @@ import (
 	"container/heap"
 	"github.com/name5566/leaf/log"
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 // session
@@ -43,34 +44,31 @@ func (h *SessionHeap) Pop() interface{} {
 	return s
 }
 
-// dial context
+// one DialContext per goroutine (goroutine not safe)
 type DialContext struct {
 	sessions SessionHeap
 }
 
-type DialInfo struct {
-	Url        string
-	SessionNum int
-}
-
-func Dial(info DialInfo) (*DialContext, error) {
-	if info.SessionNum <= 0 {
-		info.SessionNum = 100
-		log.Release("invalid SessionNum, reset to %v", info.SessionNum)
+// goroutine safe
+func Dial(url string, sessionNum int) (*DialContext, error) {
+	if sessionNum <= 0 {
+		sessionNum = 100
+		log.Release("invalid sessionNum, reset to %v", sessionNum)
 	}
 
-	s, err := mgo.Dial(info.Url)
+	s, err := mgo.Dial(url)
 	if err != nil {
 		return nil, err
 	}
 
 	c := new(DialContext)
-	c.sessions = make(SessionHeap, info.SessionNum)
+
+	// sessions
+	c.sessions = make(SessionHeap, sessionNum)
 	c.sessions[0] = &Session{s, 0, 0}
-	for i := 1; i < info.SessionNum; i++ {
+	for i := 1; i < sessionNum; i++ {
 		c.sessions[i] = &Session{s.New(), 0, i}
 	}
-
 	heap.Init(&c.sessions)
 
 	return c, nil
@@ -93,4 +91,45 @@ func (c *DialContext) Ref() *Session {
 func (c *DialContext) UnRef(s *Session) {
 	s.ref--
 	heap.Fix(&c.sessions, s.index)
+}
+
+func (c *DialContext) EnsureCounter(db string, collection string, id string) error {
+	s := c.Ref()
+	defer c.UnRef(s)
+
+	err := s.DB(db).C(collection).Insert(bson.M{
+		"_id": id,
+		"seq": 0,
+	})
+	if mgo.IsDup(err) {
+		return nil
+	} else {
+		return err
+	}
+}
+
+func (c *DialContext) NextSeq(db string, collection string, id string) (int, error) {
+	s := c.Ref()
+	defer c.UnRef(s)
+
+	var res struct {
+		Seq int
+	}
+	_, err := s.DB(db).C(collection).FindId(id).Apply(mgo.Change{
+		Update:    bson.M{"$inc": bson.M{"seq": 1}},
+		ReturnNew: true,
+	}, &res)
+
+	return res.Seq, err
+}
+
+func (c *DialContext) EnsureIndex(db string, collection string, key []string) error {
+	s := c.Ref()
+	defer c.UnRef(s)
+
+	return s.DB(db).C(collection).EnsureIndex(mgo.Index{
+		Key:    key,
+		Unique: true,
+		Sparse: true,
+	})
 }
