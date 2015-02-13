997b668910ae5a40dd74bba405b304592e9cf339
name5566 <name5566@gmail.com>
2015-02-13 13:20:48 +0800

    example for mongodb

---
+++ db/mongodb/example_test.go
@@ -0,0 +1,48 @@
+package mongodb_test
+
+import (
+	"fmt"
+	"github.com/name5566/leaf/db/mongodb"
+	"gopkg.in/mgo.v2"
+)
+
+func Example() {
+	c, err := mongodb.Dial("localhost", 10)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer c.Close()
+
+	// session
+	s := c.Ref()
+	defer c.UnRef(s)
+	err = s.DB("test").C("counters").RemoveId("test")
+	if err != nil && err != mgo.ErrNotFound {
+		fmt.Println(err)
+		return
+	}
+
+	// auto increment
+	err = c.EnsureCounter("test", "counters", "test")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for i := 0; i < 3; i++ {
+		id, err := c.NextSeq("test", "counters", "test")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(id)
+	}
+
+	// index
+	c.EnsureIndex("test", "counters", []string{"key1"})
+
+	// Output:
+	// 1
+	// 2
+	// 3
+}
