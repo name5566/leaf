2a60ed5da08dfef270ce0c841a93405a169063a8
name5566 <name5566@gmail.com>
2015-02-25 14:29:20 +0800

    CronField -> uint64

--- timer/cron.go
+++ timer/cron.go
@@ -1,16 +1,13 @@
 package timer
 
+// reference: https://github.com/robfig/cron
 import (
 	"fmt"
 	"math"
 	"strconv"
 	"strings"
-	_ "time"
+	"time"
 )
-
-type CronField uint64
-
-const maxCronField = math.MaxUint64
 
 // Field name   | Mandatory? | Allowed values | Allowed special characters
 // ----------   | ---------- | -------------- | --------------------------
@@ -21,12 +18,12 @@ const maxCronField = math.MaxUint64
 // Month        | Yes        | 1-12           | * / , -
 // Day of week  | Yes        | 0-6            | * / , -
 type CronExpr struct {
-	sec   CronField
-	min   CronField
-	hour  CronField
-	dom   CronField
-	month CronField
-	dow   CronField
+	sec   uint64
+	min   uint64
+	hour  uint64
+	dom   uint64
+	month uint64
+	dow   uint64
 }
 
 func NewCronExpr(expr string) (cronExpr *CronExpr, err error) {
@@ -44,47 +41,47 @@ func NewCronExpr(expr string) (cronExpr *CronExpr, err error) {
 	// Seconds
 	cronExpr.sec, err = parseCronField(fields[0], 0, 59)
 	if err != nil {
-		goto parseError
+		goto onError
 	}
 	// Minutes
 	cronExpr.min, err = parseCronField(fields[1], 0, 59)
 	if err != nil {
-		goto parseError
+		goto onError
 	}
 	// Hours
 	cronExpr.hour, err = parseCronField(fields[2], 0, 23)
 	if err != nil {
-		goto parseError
+		goto onError
 	}
 	// Day of month
 	cronExpr.dom, err = parseCronField(fields[3], 1, 31)
 	if err != nil {
-		goto parseError
+		goto onError
 	}
 	// Month
 	cronExpr.month, err = parseCronField(fields[4], 1, 12)
 	if err != nil {
-		goto parseError
+		goto onError
 	}
 	// Day of week
 	cronExpr.dow, err = parseCronField(fields[5], 0, 6)
 	if err != nil {
-		goto parseError
+		goto onError
 	}
 	return
 
-parseError:
+onError:
 	err = fmt.Errorf("invalid expr %v: %v", expr, err)
 	return
 }
 
 // 1. *
 // 2. num
-// 3. */num
-// 4. num-num
-// 5. num-num/num
-// 6. num/num (means num-max/num)
-func parseCronField(field string, min int, max int) (cronField CronField, err error) {
+// 3. num-num
+// 4. */num
+// 5. num/num (means num-max/num)
+// 6. num-num/num
+func parseCronField(field string, min int, max int) (cronField uint64, err error) {
 	fields := strings.Split(field, ",")
 	for _, field := range fields {
 		rangeAndIncr := strings.Split(field, "/")
@@ -155,14 +152,14 @@ func parseCronField(field string, min int, max int) (cronField CronField, err er
 				return
 			}
 			if incr <= 0 {
-				err = fmt.Errorf("invalid increment: %v", incr)
+				err = fmt.Errorf("invalid increment: %v", rangeAndIncr[1])
 				return
 			}
 		}
 
 		// cronField
 		if incr == 1 {
-			cronField |= ^(maxCronField << uint(end+1)) & (maxCronField << uint(start))
+			cronField |= ^(math.MaxUint64 << uint(end+1)) & (math.MaxUint64 << uint(start))
 		} else {
 			for i := start; i <= end; i += incr {
 				cronField |= 1 << uint(i)
@@ -171,4 +168,8 @@ func parseCronField(field string, min int, max int) (cronField CronField, err er
 	}
 
 	return
+}
+
+func (e *CronExpr) Next(t time.Time) time.Time {
+	return time.Time{}
 }
