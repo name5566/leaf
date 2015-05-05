6712e2830dfcc12a150cc86adc33b0836c44f376
name5566 <name5566@gmail.com>
2015-05-05 18:51:18 +0800

    json message support

--- network/json/json.go
+++ network/json/json.go
@@ -1,3 +1,139 @@
 package json
 
-// not implemented
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/name5566/leaf/chanrpc"
+	"github.com/name5566/leaf/log"
+	"reflect"
+)
+
+type Processor struct {
+	msgInfo map[string]*MsgInfo
+}
+
+type MsgInfo struct {
+	msgType    reflect.Type
+	msgRouter  *chanrpc.Server
+	msgHandler MsgHandler
+}
+
+type MsgHandler func([]interface{})
+
+func NewProcessor() *Processor {
+	p := new(Processor)
+	p.msgInfo = make(map[string]*MsgInfo)
+	return p
+}
+
+// It's dangerous to call the method on routing or marshaling (unmarshaling)
+func (p *Processor) Register(msg interface{}) {
+	msgType := reflect.TypeOf(msg)
+	if msgType == nil || msgType.Kind() != reflect.Ptr {
+		log.Fatal("json message pointer required")
+	}
+	msgID := msgType.Elem().Name()
+	if msgID == "" {
+		log.Fatal("unnamed json message")
+	}
+	if _, ok := p.msgInfo[msgID]; ok {
+		log.Fatal("message %v is already registered", msgID)
+	}
+
+	i := new(MsgInfo)
+	i.msgType = msgType
+	p.msgInfo[msgID] = i
+}
+
+// It's dangerous to call the method on routing or marshaling (unmarshaling)
+func (p *Processor) SetRouter(msg interface{}, msgRouter *chanrpc.Server) {
+	msgType := reflect.TypeOf(msg)
+	if msgType == nil || msgType.Kind() != reflect.Ptr {
+		log.Fatal("json message pointer required")
+	}
+	msgID := msgType.Elem().Name()
+	i, ok := p.msgInfo[msgID]
+	if !ok {
+		log.Fatal("message %v not registered", msgID)
+	}
+
+	i.msgRouter = msgRouter
+}
+
+// It's dangerous to call the method on routing or marshaling (unmarshaling)
+func (p *Processor) SetHandler(msg interface{}, msgHandler MsgHandler) {
+	msgType := reflect.TypeOf(msg)
+	if msgType == nil || msgType.Kind() != reflect.Ptr {
+		log.Fatal("json message pointer required")
+	}
+	msgID := msgType.Elem().Name()
+	i, ok := p.msgInfo[msgID]
+	if !ok {
+		log.Fatal("message %v not registered", msgID)
+	}
+
+	i.msgHandler = msgHandler
+}
+
+// goroutine safe
+func (p *Processor) Route(msg interface{}, userData interface{}) error {
+	msgType := reflect.TypeOf(msg)
+	if msgType == nil || msgType.Kind() != reflect.Ptr {
+		return errors.New("json message pointer required")
+	}
+	msgID := msgType.Elem().Name()
+	i, ok := p.msgInfo[msgID]
+	if !ok {
+		return fmt.Errorf("message %v not registered", msgID)
+	}
+
+	if i.msgHandler != nil {
+		i.msgHandler([]interface{}{msg, userData})
+	}
+	if i.msgRouter != nil {
+		i.msgRouter.Go(msgType, msg, userData)
+	}
+	return nil
+}
+
+// goroutine safe
+func (p *Processor) Unmarshal(data []byte) (interface{}, error) {
+	var m map[string]json.RawMessage
+	err := json.Unmarshal(data, &m)
+	if err != nil {
+		return nil, err
+	}
+	if len(m) != 1 {
+		return nil, errors.New("invalid json data")
+	}
+
+	for msgID, data := range m {
+		i, ok := p.msgInfo[msgID]
+		if !ok {
+			return nil, fmt.Errorf("message %v not registered", msgID)
+		}
+
+		// msg
+		msg := reflect.New(i.msgType.Elem()).Interface()
+		return msg, json.Unmarshal(data, msg)
+	}
+
+	panic("bug")
+}
+
+// goroutine safe
+func (p *Processor) Marshal(msg interface{}) ([]byte, error) {
+	msgType := reflect.TypeOf(msg)
+	if msgType == nil || msgType.Kind() != reflect.Ptr {
+		return nil, errors.New("json message pointer required")
+	}
+	msgID := msgType.Elem().Name()
+	if _, ok := p.msgInfo[msgID]; !ok {
+		return nil, fmt.Errorf("message %v not registered", msgID)
+	}
+
+	// data
+	m := map[string]interface{}{msgID: msg}
+	return json.Marshal(m)
+}
