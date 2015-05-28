10f91173f13e91d0bb1e2582c67eb1b0101d7bd3
name5566 <name5566@gmail.com>
2015-05-28 18:12:22 +0800

    linear go support

--- go/go.go
+++ go/go.go
@@ -1,9 +1,26 @@
 package g
 
+import (
+	"container/list"
+	"sync"
+)
+
 // one Go per goroutine (goroutine not safe)
 type Go struct {
 	ChanCb    chan func()
 	pendingGo int
+}
+
+type LinearGo struct {
+	f  func()
+	cb func()
+}
+
+type LinearContext struct {
+	g              *Go
+	linearGo       *list.List
+	mutexLinearGo  sync.Mutex
+	mutexExecution sync.Mutex
 }
 
 func New(l int) *Go {
@@ -33,4 +50,31 @@ func (g *Go) Close() {
 	for g.pendingGo > 0 {
 		g.Cb(<-g.ChanCb)
 	}
+}
+
+func (g *Go) NewLinearContext() *LinearContext {
+	c := new(LinearContext)
+	c.g = g
+	c.linearGo = list.New()
+	return c
+}
+
+func (c *LinearContext) Go(f func(), cb func()) {
+	c.g.pendingGo++
+
+	c.mutexLinearGo.Lock()
+	c.linearGo.PushBack(&LinearGo{f: f, cb: cb})
+	c.mutexLinearGo.Unlock()
+
+	go func() {
+		c.mutexExecution.Lock()
+		defer c.mutexExecution.Unlock()
+
+		c.mutexLinearGo.Lock()
+		lg := c.linearGo.Remove(c.linearGo.Front()).(*LinearGo)
+		c.mutexLinearGo.Unlock()
+
+		lg.f()
+		c.g.ChanCb <- lg.cb
+	}()
 }
