4869522f70eecdfadeced26554a44f79998262cc
name5566 <name5566@gmail.com>
2015-07-31 18:28:29 +0800

    RandIntervalN [b1, b2]

--- util/rand.go
+++ util/rand.go
@@ -49,3 +49,43 @@ func RandInterval(b1, b2 int32) int32 {
 	}
 	return int32(rand.Int63n(max-min+1) + min)
 }
+
+func RandIntervalN(b1, b2 int32, n uint32) []int32 {
+	if b1 == b2 {
+		return []int32{b1}
+	}
+
+	min, max := int64(b1), int64(b2)
+	if min > max {
+		min, max = max, min
+	}
+	l := max - min + 1
+	if int64(n) > l {
+		n = uint32(l)
+	}
+
+	r := make([]int32, n)
+	m := make(map[int32]int32)
+	for i := uint32(0); i < n; i++ {
+		v := int32(rand.Int63n(l) + min)
+
+		if mv, ok := m[v]; ok {
+			r[i] = mv
+		} else {
+			r[i] = v
+		}
+
+		lv := int32(l - 1 + min)
+		if v != lv {
+			if mv, ok := m[lv]; ok {
+				m[v] = mv
+			} else {
+				m[v] = lv
+			}
+		}
+
+		l--
+	}
+
+	return r
+}
