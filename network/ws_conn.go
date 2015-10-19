d5af92961fd9a3dd8d7bdfb06117a2f19c930df7
name5566 <name5566@gmail.com>
2015-10-19 10:14:12 +0800

    basic WSServer structure

--- network/ws_conn.go
+++ network/ws_conn.go
@@ -1,1 +1,12 @@
 package network
+
+import (
+	"github.com/gorilla/websocket"
+	_ "github.com/name5566/leaf/log"
+	_ "sync"
+)
+
+type WebsocketConnSet map[*websocket.Conn]struct{}
+
+type WSConn struct {
+}
--- network/ws_server.go
+++ network/ws_server.go
@@ -1,25 +1,43 @@
 package network
 
 import (
-	_ "github.com/gorilla/websocket"
+	"github.com/gorilla/websocket"
 	"github.com/name5566/leaf/log"
 	"net"
 	"net/http"
-	_ "sync"
+	"sync"
 	"time"
 )
 
 type WSServer struct {
-	Addr        string
-	HTTPTimeout time.Duration
+	Addr            string
+	MaxConnNum      int
+	PendingWriteNum int
+	NewAgent        func(*WSConn) Agent
+	HTTPTimeout     time.Duration
+	ln              net.Listener
+	handler         *WSHandler
 }
 
 type WSHandler struct {
-	*WSServer
+	maxConnNum      int
+	pendingWriteNum int
+	upgrader        websocket.Upgrader
+	conns           WebsocketConnSet
+	mutexConns      sync.Mutex
+	wg              sync.WaitGroup
 }
 
 func (handler *WSHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", 405)
+		return
+	}
+	conn, err := handler.upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Debug("upgrade error: %v", err)
+		return
+	}
 }
 
 func (server *WSServer) Start() {
@@ -28,27 +46,53 @@ func (server *WSServer) Start() {
 		log.Fatal("%v", err)
 	}
 
+	if server.MaxConnNum <= 0 {
+		server.MaxConnNum = 100
+		log.Release("invalid MaxConnNum, reset to %v", server.MaxConnNum)
+	}
+	if server.PendingWriteNum <= 0 {
+		server.PendingWriteNum = 100
+		log.Release("invalid PendingWriteNum, reset to %v", server.PendingWriteNum)
+	}
+	if server.NewAgent == nil {
+		log.Fatal("NewAgent must not be nil")
+	}
 	if server.HTTPTimeout <= 0 {
 		server.HTTPTimeout = 10 * time.Second
 		log.Release("invalid HTTPTimeout, reset to %v", server.HTTPTimeout)
 	}
 
+	server.ln = ln
+	server.handler = &WSHandler{
+		maxConnNum:      server.MaxConnNum,
+		pendingWriteNum: server.PendingWriteNum,
+		conns:           make(WebsocketConnSet),
+		upgrader: websocket.Upgrader{
+			HandshakeTimeout: server.HTTPTimeout,
+			CheckOrigin:      func(_ *http.Request) bool { return true },
+		},
+	}
+
 	httpServer := &http.Server{
 		Addr:           server.Addr,
-		Handler:        &WSHandler{server},
+		Handler:        server.handler,
 		ReadTimeout:    server.HTTPTimeout,
 		WriteTimeout:   server.HTTPTimeout,
 		MaxHeaderBytes: 1024,
 	}
 
-	go func() {
-		err := httpServer.Serve(ln)
-		if err != nil {
-			log.Error("http serve error: %v", err)
-		}
-	}()
+	go httpServer.Serve(ln)
 }
 
 func (server *WSServer) Close() {
+	server.ln.Close()
 
+	server.handler.mutexConns.Lock()
+	for conn := range server.handler.conns {
+		conn.Close()
+	}
+	server.handler.conns = nil
+	server.handler.mutexConns.Unlock()
+
+	server.handler.wg.Wait()
 }
